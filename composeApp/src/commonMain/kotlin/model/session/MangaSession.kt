0ba49eb9ac1280af17cddd7be5f3d46094da0209
kite1412 <frozradit@gmail.com>
2024-07-12 10:23:51 +0800

    Update and fix auto refresh strategy

--- composeApp/src/commonMain/kotlin/model/session/MangaSession.kt
+++ composeApp/src/commonMain/kotlin/model/session/MangaSession.kt
@@ -1,7 +1,7 @@
 package model.session
 
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.snapshots.SnapshotStateMap
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import api.mangadex.model.response.ListResponse
 import api.mangadex.model.response.attribute.MangaAttributes
 import api.mangadex.util.ApiConstant
@@ -11,5 +11,5 @@ data class MangaSession(
     override var url: String = ApiConstant.MANGA_ENDPOINT,
     override val queries: MutableMap<String, Any> = mutableMapOf(),
     override var response: ListResponse<MangaAttributes> = ListResponse(),
-    override var data: SnapshotStateMap<String, Manga> = mutableStateMapOf()
-): Session<String, Manga, MangaAttributes>
+    override var data: SnapshotStateList<Manga> = mutableStateListOf()
+): Session<Manga, MangaAttributes>
--- composeApp/src/commonMain/kotlin/util/session_handler/SessionHandler.kt
+++ composeApp/src/commonMain/kotlin/util/session_handler/SessionHandler.kt
@@ -3,9 +3,9 @@ package util.session_handler
 import api.mangadex.service.MangaDex
 import model.session.Session
 
-interface SessionHandler<K, T, ATTR> {
-    val session: Session<K, T, ATTR>
+interface SessionHandler<T, ATTR> {
+    val session: Session<T, ATTR>
     val mangaDex: MangaDex
 
-    suspend fun updateSession(finish: (Boolean) -> Unit)
+    suspend fun updateSession(onFinish: (Boolean, Session<T, ATTR>?) -> Unit)
 }
