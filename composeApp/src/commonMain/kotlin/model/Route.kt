c6cd1e1cbc7e61f728647c67f782904b8f48b887
kite1412 <frozradit@gmail.com>
2024-07-30 11:00:14 +0800

    Update navigation bar

--- composeApp/src/commonMain/kotlin/model/Route.kt
+++ composeApp/src/commonMain/kotlin/model/Route.kt
@@ -1,15 +1,8 @@
 package model
 
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-
-data class ItemColor(
-    val selected: Color,
-    val unselected: Color
-)
 
 data class Route(
     val name: String,
     val icon: ImageVector,
-    val color: ItemColor
 )
